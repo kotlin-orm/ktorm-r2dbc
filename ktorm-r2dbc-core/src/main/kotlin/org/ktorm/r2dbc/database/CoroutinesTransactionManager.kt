47c297eaa2183aa79633e58e2d5316cf46d72c61
vincent.liu <me@liuwj.me>
2021-02-02 23:34:20 +0800

    refactor coroutines transaction manager

--- ktorm-r2dbc-core/src/main/kotlin/org/ktorm/r2dbc/database/CoroutinesTransactionManager.kt
+++ ktorm-r2dbc-core/src/main/kotlin/org/ktorm/r2dbc/database/CoroutinesTransactionManager.kt
@@ -25,50 +25,60 @@ public class CoroutinesTransactionManager(
             throw IllegalStateException("There is already a transaction in the current context.")
         }
 
-        val connection = connectionFactory.create().awaitSingle()
+        val transaction = TransactionImpl(connectionFactory.create().awaitSingle(), isolation)
+        currentTransaction.set(transaction)
+        transaction.begin()
+        return transaction
+    }
 
-        val originIsolation = connection.transactionIsolationLevel
-        if (isolation != null && isolation != originIsolation) {
-            connection.setTransactionIsolationLevel(isolation).awaitSingleOrNull()
+    private inner class TransactionImpl(
+        override val connection: Connection,
+        private val desiredIsolation: IsolationLevel?
+    ) : Transaction {
+        private val originIsolation = connection.transactionIsolationLevel
+        private val originAutoCommit = connection.isAutoCommit
+
+        suspend fun begin() {
+            try {
+                if (desiredIsolation != null && desiredIsolation != originIsolation) {
+                    connection.setTransactionIsolationLevel(desiredIsolation).awaitSingleOrNull()
+                }
+                if (originAutoCommit) {
+                    connection.setAutoCommit(false).awaitSingleOrNull()
+                }
+
+                connection.beginTransaction().awaitSingleOrNull()
+            } catch (e: Throwable) {
+                close()
+                throw e
+            }
         }
 
-        val originAutoCommit = connection.isAutoCommit
-        if (originAutoCommit) {
-            connection.setAutoCommit(false).awaitSingleOrNull()
+        override suspend fun commit() {
+            connection.commitTransaction().awaitSingleOrNull()
         }
 
-        val transaction = object : Transaction {
-            override val connection: Connection = connection
+        override suspend fun rollback() {
+            connection.rollbackTransaction().awaitSingleOrNull()
+        }
 
-            override suspend fun commit() {
-                connection.commitTransaction().awaitSingleOrNull()
-            }
-
-            override suspend fun rollback() {
-                connection.rollbackTransaction().awaitSingleOrNull()
-            }
-
-            override suspend fun close() {
+        override suspend fun close() {
+            try {
+                if (desiredIsolation != null && desiredIsolation != originIsolation) {
+                    connection.setTransactionIsolationLevel(originIsolation).awaitSingleOrNull()
+                }
+                if (originAutoCommit) {
+                    connection.setAutoCommit(true).awaitSingleOrNull()
+                }
+            } catch (_: Throwable) {
+            } finally {
                 try {
-                    if (isolation != null && isolation != originIsolation) {
-                        connection.setTransactionIsolationLevel(originIsolation).awaitSingleOrNull()
-                    }
-                    if (originAutoCommit) {
-                        connection.setAutoCommit(true).awaitSingleOrNull()
-                    }
+                    connection.close().awaitSingleOrNull()
                 } catch (_: Throwable) {
                 } finally {
-                    try {
-                        connection.close().awaitSingleOrNull()
-                    } catch (_: Throwable) {
-                    } finally {
-                        currentTransaction.remove()
-                    }
+                    currentTransaction.remove()
                 }
             }
         }
-
-        currentTransaction.set(transaction)
-        return transaction
     }
 }
